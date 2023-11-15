d517e7655e4f5b3cf0991bcb35f32b1960e8c8d6
Heitor Scalabrini <47569622+heitorsam@users.noreply.github.com>
2023-11-15 08:54:18 -0300

    Integração form HTML externo e Mysql

--- src/routes.js
+++ src/routes.js
@@ -2,17 +2,49 @@
 const express = require('express');
 const router = express.Router();
 
-//ROTA COM PARAMETRO
-router.get('/users/:id', (req, res) => {
-  const userId = req.params.id;
-  res.send(`Visualizando perfil do usuário ${userId}`);
-});
+//IMPORTACAO MYSQL2 QUE ACEITA PROMISES
+const mysql = require('mysql2/promise'); 
 
-//ROTA COM FORMULARIO
-router.post('/submit', (req, res) => {
-  const formData = req.body;
-  res.send('Dados do formulário recebidos com sucesso!');
-});
+//IMPORTACAO DB MYSQL
+const dbMysql = require('./dbMysql');
+
+//CRIANDO ASYNC MIDDLEWARE
+const asyncMiddleware = fn => (req, res, next) => {
+  Promise.resolve(fn(req, res, next)).catch(next);
+};
+
+//RECEBENDO DADOS DO FORMULARIO CONTATO FACTUN
+router.post('/api/formcontato', asyncMiddleware(async (req, res) => {
+
+  const dadosform = req.body;
+  console.log('Dados recebidos:', dadosform);
+
+  //CONECTA AO BANCO
+  const connection = await mysql.createConnection(dbMysql);
+
+  //INSERINDO DADOS NO MYSQL
+  try {
+
+    const result = await connection.execute(
+      'INSERT INTO FALE_CONOSCO (NOME, TELEFONE, EMAIL, ID_ASSUNTO, MENSAGEM) VALUES (?, ?, ?, ?, ?)',
+      [dadosform.nome, dadosform.telefone, dadosform.email, dadosform.id_assunto, dadosform.mensagem]
+    );
+
+    console.log('Inserido com sucesso:', result);
+    res.json({ status: 'success', mensagem: 'Dados inseridos com sucesso!' });
+
+  } catch (error) {
+
+    //EM CASO DE ERRO
+    console.error('Erro ao inserir no banco de dados:', error);
+    res.status(500).json({ status: 'error', mensagem: 'Erro ao inserir no banco de dados.' });
+
+  }
+
+   //DESCONECTA DO BANCO
+   connection.end();
+
+}));
 
 //EXPORTS 
 module.exports = router;
--- src/server.js
+++ src/server.js
@@ -1,15 +1,19 @@
 //IMPORTACOES
 const express = require('express');
-const fs = require('fs');
-const bodyParser = require('body-parser');
 const app = express();
 
-//CRIA ARQUIVO DE LOG
+//ARQUIVOS
+const fs = require('fs');
 const logFile = fs.createWriteStream('log.txt', { flags: 'a' });
 
+//CROSS PLATAFORMAS
+const cors = require('cors');
+app.use(cors()); 
+
 // MIDDLEWARE FORMULARIO HTML
+const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+//app.use(bodyParser.json());
 
 //IMPORTA ROTAS
 const routes = require('./routes');
@@ -28,7 +32,7 @@ app.use((req, res) => {
 //INICIA SERVIDOR NA PORTA 8099
 const PORT = 8099;
 app.listen(PORT, () => {
-  console.log('Iniciado server na porta: ${PORT}');
+  console.log('Iniciado server na porta: ' + PORT);
 });
 
 //EXPORTS
